6cc6c23b4485568411d80f144174540cf6fc274a
maram-p <mario.medinap@outlook.com>
2021-09-20 00:05:58 -0500

    *se creo ruta para obtener usuarios *se creo ruta para eliminar usuario *se creo ruta para asignar un rol a un usuario **se creo ruta para crear un usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = Router();
 
 const {getProducts, createProducts} = require('../controllers/productosController')
 const {getRoles, createRoles} = require('../controllers/rolesController')
+const {getUsers, createUsers, addRole, deletedUser} = require('../controllers/usersController')
 
 //products
 router.get('/products', getProducts);
@@ -10,7 +11,10 @@ router.post('/products', createProducts);
 //roles
 router.get('/roles', getRoles);
 router.post('/roles', createRoles);
-
-
+//users
+router.get('/users', getUsers);
+router.post('/users', createUsers);
+router.put('/users/:id/add-role', addRole);
+router.delete('/users/:id/delete', deletedUser);
 
 module.exports = router;
